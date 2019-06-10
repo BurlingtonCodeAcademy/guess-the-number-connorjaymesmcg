26a4a2179fdaeb3823091dad08b8fa0322e8a94d
Connor McGinnis <connorjaymesmcg@gmail.com>
2019-06-10 12:15:28 -0400

    guess-the-number-final

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const readline = require('readline');
+const readline = require("readline"); // readline is boilerplate code
 const rl = readline.createInterface(process.stdin, process.stdout);
+let questionText = "Can I guess your number?";
 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
@@ -8,11 +9,83 @@ function ask(questionText) {
 }
 
 start();
-
+//begin function
 async function start() {
-  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-  console.log('You entered: ' + secretNumber);
+  console.log(
+    "\n" +
+      "Sup, fam? Let's play a game where you (dumb human) think of a number and I (the wicked smaht computer) try to guess it." +
+      "\n" +
+      "Ready?" +
+      "\n" +
+      "Go!"
+  );
+  let min = 0;
+  let max = 100;
+  let computerNumber = Math.floor((min + max) / 2);
+  let answer = await ask(
+    "Is your number " +
+      computerNumber +
+      "? " +
+      "\n" +
+      "\n" +
+      "Enter 'Yes' or 'No' "
+  );
+  //answer = answer.toLowerCase();
+
+  while (answer.toLowerCase() != "yes") {
+    //while answer is NOT yes run the following while loop \/ \/ \/ \/ \/ \/
+    // `await` means "wait for the following thing to happen"
+    //when you use `await` inside a function, you must use `async` to define that function
+
+    //if (answer.toLowerCase().includes("yes")) {
+    // ".includes" looks at ASCII instead of string
+    //console.log("I win!");
+    //process.exit();
+    //console.log('You entered: ');
+    //}
+
+    if (answer.toLowerCase().includes("no")) {
+      let wrongAnswer = await ask("Is it higher or lower? ");
+      if (wrongAnswer.toLowerCase().includes("higher")) {
+        min = computerNumber;
+        computerNumber = Math.floor((min + max) / 2);
+        answer = await ask("Is your number " + computerNumber + "? ");
+      }
+      if (answer.toLowerCase().includes("no")) {
+        if (wrongAnswer.toLowerCase().includes("lower")) {
+          max = computerNumber;
+          computerNumber = Math.floor((min + max) / 2);
+          answer = await ask("Is your number " + computerNumber + "? ");
+        }
+      }
+
+      //if (answer.toLowerCase().includes !== ("yes" || "no" || "higher" || "lower")) {
+      //console.log("I do not recognize the response.")
+      //answer = await ask("Is your number " + computerNumber + "? ")
+      //}
+    }
+
+    //computerNumber = randomInteger(min, max);
+    //answer = await ask("Is your number " + computerNumber + "? ");
+  } // END OF WHILE (answer is NOT yes) LOOP
+
+  //console.log('You entered: ');
+
+  // If statements -- always evaluating top to bottom
+  // will always hit the first one -- only moves to second if false.
+
   // Now try and complete the program.
+  console.log(
+    "\n" +
+      "I have guessed your number." +
+      "\n" +
+      "I am all powerful." +
+      "\n" +
+      "Program ending..." +
+      "\n" +
+      "Goodbye." +
+      "\n"
+  );
   process.exit();
 }
+
